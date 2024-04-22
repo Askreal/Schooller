fb0ef99bde1ca51579ab90f2465bd1601454def4
LackSweet <plainakalin@gmail.com>
2024-04-22 22:15:05 +0700

    Fix timeout 2

--- src/app/display_students/route.js
+++ src/app/display_students/route.js
@@ -5,7 +5,7 @@ import Student from "../../../models/students";
 export async function GET() {
     try {
         await connectMongoDB();
-        const allStudents = await Student.findOne({});
+        const allStudents = await Student.findOne();
         return NextResponse.json(allStudents, { status: 200 });
     } catch (error) {
         return NextResponse.json({ message: "An error occured while fetching all students." }, { status: 500 });
