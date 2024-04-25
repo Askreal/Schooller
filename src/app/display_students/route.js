c918c0ff3a77a4967fe2c97a16069bd719abef56
LackSweet <plainakalin@gmail.com>
2024-04-25 13:53:54 +0700

    Add condition in find

--- src/app/display_students/route.js
+++ src/app/display_students/route.js
@@ -4,10 +4,16 @@ import Student from "../../../models/students";
 
 export async function GET() {
     try {
+        // Connect to the MongoDB database using connection pooling
         await connectMongoDB();
-        const allStudents = await Student.find();
+
+        // Specify projection to only retrieve necessary fields
+        const allStudents = await Student.find({}, { _id: 0, name: 1, age: 1 });
+
+        // Return response with status 200 and JSON data
         return Response.json(allStudents, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ message: "An error occured while fetching all students." }, { status: 500 });
+        // Return error response with status 500 if an error occurs
+        return NextResponse.json({ message: "An error occurred while fetching all students." }, { status: 500 });
     }
 }
